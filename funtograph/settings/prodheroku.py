b0804bf44377b9bf3b025c0d48d6b505e2373872
nnikolic72 <nnenad@gmail.com>
2015-03-11 11:27:31 +0100

    added production settings file

---
+++ funtograph/settings/prodheroku.py
@@ -0,0 +1,29 @@
+from funtograph.settings.base import *
+
+__author__ = 'tanja'
+
+import os
+import dj_database_url
+
+#from memcacheify import memcacheify  # @UnresolvedImport
+#CACHES = memcacheify()
+
+
+SECRET_KEY =  os.environ['SECRET_KEY']
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../../static'),
+)
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite')
+}
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
+MIDDLEWARE_CLASSES += ('django.middleware.clickjacking.XFrameOptionsMiddleware',)
